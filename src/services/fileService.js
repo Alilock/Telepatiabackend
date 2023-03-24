6dae72ef7ab55f09b58fbaf4779083f2f8d93337
Ali <105427281+Alilock@users.noreply.github.com>
2023-03-24 04:29:41 +0400

    fixed

--- src/services/fileService.js
+++ src/services/fileService.js
@@ -6,7 +6,7 @@ const fileSave = (image) => {
     let paths = [];
     let ext = path.extname(image.name);
     let newName = `${Date.now()}_${Math.floor(Math.random() * 1000)}${ext}`;
-    let newPath = path.join(__dirname, './assets/uploads', newName);
+    let newPath = path.join(__dirname, '../assets/uploads', newName);
     image.mv(newPath, err => {
         console.log('err', err);
     })

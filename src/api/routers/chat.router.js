506d89f381766d2371d9261e95e5afb5533af637
Ali <105427281+Alilock@users.noreply.github.com>
2023-03-27 20:54:31 +0400

    chatprofilepicture populate

--- src/api/routers/chat.router.js
+++ src/api/routers/chat.router.js
@@ -2,10 +2,14 @@ const express = require('express');
 const router = express.Router();
 const Chat = require('../../models/chat.model');
 
-router.get('/:senderId/:receiverId', async (req, res) => {
-    const { senderId, receiverId } = req.params;
-    const chat = await Chat.findOne({ users: { $all: [senderId, receiverId] } }).populate('messages.sender messages.receiver', 'username');
-    res.json(chat);
+router.get('/:senderId/:receiverId', async (req, res, next) => {
+    try {
+        const { senderId, receiverId } = req.params;
+        const chat = await Chat.findOne({ users: { $all: [senderId, receiverId] } }).populate('messages.sender messages.receiver', 'username profilePicture');
+        res.json(chat);
+    } catch (error) {
+        next(error)
+    }
 });
 
 module.exports = router;
